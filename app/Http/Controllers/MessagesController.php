418887a43ad3fb1c08d00b4935b505ded5eccfd3
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-02-27 12:52:31 -0500

    Validación de formulario de mensaje con formRequest en página principal

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Message;
+use App\Http\Requests\CreateMessageRequest;
 
 class MessagesController extends Controller
 {
@@ -14,7 +15,7 @@ class MessagesController extends Controller
       ]);
     }
 
-    public function create(Request $request){
+    public function create(CreateMessageRequest $request){
       dd($request->all());
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,14 @@
         <form action="messages/create" method="post">
           <div class="form-group">
             {{ csrf_field() }}
-            <input type="text" name="message" class="form-control" placeholder="¿Qué estás pensando?">
+            <input type="text" name="message" class="form-control @if($errors->has('message')) is-invalid @endif" placeholder="¿Qué estás pensando?">
+            @if ($errors->has('message'))
+              @foreach ($errors->get('message') as $error)
+                <div class="invalid-feedback">
+                  {{ $error }}
+                </div>
+              @endforeach
+            @endif
           </div>
         </form>
       </div>
