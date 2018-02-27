c974e3489020a8bd271779d129e9171419d2ec67
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-02-27 13:07:49 -0500

    Corrección de traducción de mensaje de validación en regla max

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -53,7 +53,7 @@ return [
     'max'                  => [
         'numeric' => 'El campo :attribute no puede ser mayor que :max.',
         'file'    => 'El campo :attribute no puede ser mayor que :max kilobytes.',
-        'string'  => 'El campo :attribute no puede ser mayor que :max characters.',
+        'string'  => 'El campo :attribute no puede ser mayor que :max caracteres.',
         'array'   => 'El campo :attribute no puede tener más de :max items.',
     ],
     'mimes'                => 'El campo :attribute debe ser un archivo de tipo: :values.',
