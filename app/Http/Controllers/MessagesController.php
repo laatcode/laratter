758036083b2fa658abce2f186dbdfaf1b11ef129
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-02-27 13:18:23 -0500

    Creación de mensaje en base de datos

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -16,6 +16,11 @@ class MessagesController extends Controller
     }
 
     public function create(CreateMessageRequest $request){
-      dd($request->all());
+      $message = Message::create([
+        'content' => $request->message,
+        'image' => 'http://lorempixel.com/600/338?'.mt_rand(0, 1000),
+      ]);
+
+      return redirect('/messages/' . $message->id);
     }
 }
