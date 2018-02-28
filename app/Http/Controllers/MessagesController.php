50bdbc2ff313ff98b97cb0a87242decbd79fe5d5
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-02-28 11:51:21 -0500

    Ajuste a ruta en redirect del método crear mensajes

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -21,6 +21,6 @@ class MessagesController extends Controller
         'image' => 'http://lorempixel.com/600/338?'.mt_rand(0, 1000),
       ]);
 
-      return redirect('/messages/' . $message->id);
+      return redirect('/message/' . $message->id);
     }
 }
