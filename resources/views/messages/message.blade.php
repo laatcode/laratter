8bdee8fee2c92f6c2a29e3e1a4dbb48574bbaa79
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-03-02 14:00:48 -0500

    Ajuste a vista messages.message, cambio de etiqueta p por div y adición de fecha de creación

--- resources/views/messages/message.blade.php
+++ resources/views/messages/message.blade.php
@@ -1,6 +1,9 @@
 <img class="img-thumbnail" src="{{ $message->image }}">
-<p class="card-text">
+<div class="card-text">
   <div class="text-muted">Escrito por <a href="/{{ $message->user->username }}">{{ $message->user->name }}</a></div>
   {{ $message->content }}
   <a href="/message/{{ $message->id }}">Leer más</a>
-</p>
+</div>
+<div class="card-text text-muted float-right">
+  {{ $message->created_at }}
+</div>
