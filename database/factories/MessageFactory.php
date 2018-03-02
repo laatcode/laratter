5fc5320153def932163564136934a2d3d9c7c09a
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-03-02 14:04:52 -0500

    Ajsute a MessageFactory para generación de mensajes con fecha y hora en esta década

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -6,5 +6,7 @@ $factory->define(App\Message::class, function (Faker $faker) {
     return [
         'content' => $faker->realText(random_int(20, 160)),
         'image' => $faker->imageUrl(600, 338),
+        'created_at' => $faker->dateTimeThisDecade,
+        'updated_at' => $faker->dateTimeThisDecade,
     ];
 });
