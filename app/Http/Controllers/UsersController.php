3c94a1136b486ce852721fb73789bb71ffa7afc6
Luis Ángel Ávila Torres <luisangelavilatorres@gmail.com>
2018-03-03 17:13:29 -0500

    Método follow en controlador de Usuarios con implementación de la función de seguir

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,16 +7,31 @@ use App\User;
 
 class UsersController extends Controller
 {
+
+    private function findByUsername($username){
+      return $user = User::where('username', $username)->first();
+    }
+
     public function show($username){
-      $user = User::where('username', $username)->first();
+      $user = $this->findByUsername($username);
 
       return view('users.show', [
         'user' => $user,
       ]);
     }
 
+    public function follow($username, Request $request){
+      $user = $this->findByUsername($username);
+
+      $me = $request->user();
+      $me->follows()->attach($user);
+
+      return redirect("/$username")->withSuccess('Usuario seguido!');
+
+    }
+
     public function follows($username){
-      $user = User::where('username', $username)->first();
+      $user = $this->findByUsername($username);
 
       return view('users.follows', [
         'user' => $user,
