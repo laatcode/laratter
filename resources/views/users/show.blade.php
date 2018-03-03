80a25b71a818c0ce440ceb00241d63b3721a1eed
Luis Ángel Ávila Torres <luisangelavilatorres@gmail.com>
2018-03-03 18:32:42 -0500

    Protección de botón seguir, disponible solo para usuarios logueados

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,15 +4,17 @@
   <div class="container">
     <h1>{{ $user->name }}</h1>
 
-    <form action="/{{ $user->username }}/follow" method="post">
-      <div class="form-group">
-        @if (session('success'))
-          <span class="text-success">{{ session('success') }}</span>
-        @endif
-        {{ csrf_field() }}
-        <button class="btn btn-primary" type="submit" name="button">Seguir</button>
-      </div>
-    </form>
+    @if (Auth::check())
+      <form action="/{{ $user->username }}/follow" method="post">
+        <div class="form-group">
+          @if (session('success'))
+            <span class="text-success">{{ session('success') }}</span>
+          @endif
+          {{ csrf_field() }}
+          <button class="btn btn-primary" type="submit" name="button">Seguir</button>
+        </div>
+      </form>
+    @endif
 
     <div class="row">
       @foreach ($user->messages as $message)
