3a16a2c84cb019d5e088b78aa8c7a99a1257118a
Luis Ángel Ávila Torres <luisangelavilatorres@gmail.com>
2018-03-03 17:19:29 -0500

    Mensaje de usuario seguido al regreso del request de la función seguir

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,6 +6,9 @@
 
     <form action="/{{ $user->username }}/follow" method="post">
       <div class="form-group">
+        @if (session('success'))
+          <span class="text-success">{{ session('success') }}</span>
+        @endif
         {{ csrf_field() }}
         <button class="btn btn-primary" type="submit" name="button">Seguir</button>
       </div>
