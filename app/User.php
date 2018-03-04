6bfa283aeccf247c9c90d551774e6d6fe4df8bfe
Luis Ángel Ávila Torres <luisangelavilatorres@gmail.com>
2018-03-04 17:38:07 -0500

    Corrección de tipeo de clase SocialProfile en modelo de usuario

--- app/User.php
+++ app/User.php
@@ -44,6 +44,6 @@ class User extends Authenticatable
     }
 
     public function socialProfiles(){
-      return $this->hasMany(socialProfiles::class);
+      return $this->hasMany(SocialProfile::class);
     }
 }
