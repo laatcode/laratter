81ba19a059f43c0d6a416af09d3b69d4e9c98c60
Luis Ángel Ávila Torres <luisangelavilatorres@gmail.com>
2018-03-04 15:42:13 -0500

    Registro de usuarios en base de datos provenientes de perfiles sociales en SocialAuthController

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Socialite;
+use App\User;
+use App\SocialProfile;
 
 class SocialAuthController extends Controller
 {
@@ -19,5 +21,29 @@ class SocialAuthController extends Controller
       return view('users.facebook', [
         'user' => $user,
       ]);
+    }
+
+    public function register(Request $request){
+      $data = session('facebookUser');
+      $name = $request->name;
+      $email = $request->email;
+      $username = $request->username;
+
+      $user = User::create([
+        'name' => $name,
+        'username' => $username,
+        'email' => $email,
+        'password' => str_random(16),
+        'avatar' => $data->avatar,
+      ]);
+
+      $profile = SocialProfile::create([
+        'social_id' => $data->id,
+        'user_id' => $user->id,
+      ]);
+
+      auth()->login($user);
+
+      return redirect('/');
     }
 }
