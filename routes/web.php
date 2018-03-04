664a021883c0d626a868b49a78e5a176ce752717
Luis Ángel Ávila Torres <luisangelavilatorres@gmail.com>
2018-03-04 14:10:44 -0500

    Ruta a función de registro social

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Auth::routes();
 
 Route::get('/auth/facebook', 'SocialAuthController@facebook');
 Route::get('/auth/facebook/callback', 'SocialAuthController@callback');
+Route::post('/auth/facebook/register', 'SocialAuthController@register');
 
 Route::get('/{username}', 'UsersController@show');
 Route::get('/{username}/follows', 'UsersController@follows');
