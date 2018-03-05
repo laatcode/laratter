98861cfecd71d27af9e411462f4c225216324e20
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-03-05 13:06:04 -0500

    Formulario para envío de mensajes privados en perfil del usuario

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,10 +4,20 @@
   <div class="container">
     <h1>{{ $user->name }}</h1>
 
-    <a href="/{{ $user->username }}/follows">Sigue a <span class="badge badge-dark">{{ $user->follows->count() }}</span></a>
-    <a href="/{{ $user->username }}/followers">Seguido por <span class="badge badge-dark">{{ $user->followers->count() }}</span></a>
+    <div class="mb-3">
+      <a href="/{{ $user->username }}/follows">Sigue a <span class="badge badge-dark">{{ $user->follows->count() }}</span></a>
+      <a href="/{{ $user->username }}/followers">Seguido por <span class="badge badge-dark">{{ $user->followers->count() }}</span></a>
+    </div>
 
     @if (Auth::check())
+      <form class="form-row form-group" action="/{{ $user->username }}/dms" method="post">
+        <div class="col-10">
+          <input class="form-control" type="text" name="message">
+        </div>
+        <div class="col-2">
+          <button class="btn btn-success" type="submit">Enviar mensaje privado</button>
+        </div>
+      </form>
       @if (Auth::user()->isFollowing($user))
         <form action="/{{ $user->username }}/unfollow" method="post">
           <div class="form-group">
