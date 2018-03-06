82734016f5b109bad37f525b7ed8974c3fed52f7
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-03-06 11:00:55 -0500

    Ordenamiento por mensajes más recientes en método privateMessages del modelo de Conversaciones

--- app/Conversation.php
+++ app/Conversation.php
@@ -11,7 +11,7 @@ class Conversation extends Model
     }
 
     public function privateMessages(){
-      return $this->hasMany(PrivateMessage::class);
+      return $this->hasMany(PrivateMessage::class)->orderBy('created_at', 'desc');
     }
 
     public static function between(User $user, User $other){
