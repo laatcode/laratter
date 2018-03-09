39fca7a8ab74756165325091b7634ea5534d65d9
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-03-09 12:56:28 -0500

    Test de búsqueda de mensajes en ExampleTest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,11 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testCanSearchForMessages(){
+
+      $response = $this->get('/messages?query=Alice');
+
+      $response->assertSee('Alice');
+    }
 }
