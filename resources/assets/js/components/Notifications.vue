a01fadd33d0190719c4479ca465dc64b29f1c0ea
Luis Ángel Ávila Torres <luisangelavilatorres@gmail.com>
2018-03-17 17:30:47 -0500

    Modificación de componente Notifications de vue para incluir en el template las notificaciones nuevas y marcarlas como leídas a través de Axios

--- resources/assets/js/components/Notifications.vue
+++ resources/assets/js/components/Notifications.vue
@@ -1,7 +1,8 @@
 <template lang="html">
-  <div class="dropdown-menu">
-    <a :href="'/' + notification.data.follower.username" class="dropdown-item" v-for="notification in notifications">
-      El usuario @{{ notification.data.follower.username }} te ha seguido!
+  <div class="dropdown-menu dropdown-menu-right">
+    <span v-if="notifications==0" class="dropdown-item text-muted">No hay nuevas notificaciones</span>
+    <a v-on:click="read(notification, $event)" v-for="notification in notifications" :href="'/' + notification.data.follower.username" class="dropdown-item">
+      @{{ notification.data.follower.username }} te ha seguido! <span v-if="notification.read_at==null" class="badge badge-success">Nueva</span>
     </a>
   </div>
 </template>
@@ -15,15 +16,29 @@ export default {
     }
   },
   mounted(){
-    axios.get('/api/notifications')
-        .then(response => {
-          this.notifications = response.data;
+    this.getNotifications();
 
-          Echo.private(`App.User.${this.user}`)
-              .notification(notification => {
-                this.notifications.unshift(notification);
-              });
+    Echo.private(`App.User.${this.user}`)
+        .notification(notification => {
+          this.notifications.unshift(notification);
         });
+  },
+  methods: {
+    getNotifications: function(){
+      axios.get('/api/notifications')
+          .then(response => {
+            this.notifications = response.data;
+          });
+    },
+    read: function(notification, event){
+      if (notification.read_at==null) {
+        let _this = this;
+        axios.get('/api/readNotification/'+notification.id)
+            .then(response => {
+              _this.getNotifications();
+            });
+      }
+    }
   }
 }
 </script>
